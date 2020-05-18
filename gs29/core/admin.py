438f46ae4366af651eaedfaa0b5901dc392771db
Nitish Kumar <nitish.mahato990@gmail.com>
2020-05-18 16:13:11 +0530

    Database

---
+++ gs29/core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from core.models import Student
+# Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=('id', 'stuid','stuname','stuemail','stupass')
+#admin.site.register(Student, StudentAdmin)
